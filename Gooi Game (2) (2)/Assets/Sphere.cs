73db06202c3abd045d0690513c87c5e4d332ce8b
Caroline Dunaway <dunawaycaroline@gmail.com>
2018-10-30 13:23:42 -0400

    Movement and Speed Control
    Added a moveSpeed to the Sphere script

--- Gooi Game (2) (2)/Assets/Sphere.cs
+++ Gooi Game (2) (2)/Assets/Sphere.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class Sphere : MonoBehaviour {
 
+    public float moveSpeed;
+
 	// Use this for initialization
 	void Start () 
     {
+        moveSpeed = 1f;
+
         print("What is this place? What am I? Who am I?");	
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, Input.GetAxis("Vertical")*Time.deltaTime);	
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);	
 
     }
 }
