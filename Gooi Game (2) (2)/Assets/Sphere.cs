b872d157b26a6a30637c28ea46b0c9e7b734f9b9
Caroline Dunaway <dunawaycaroline@gmail.com>
2018-10-30 13:12:32 -0400

    Horizontal Movement
    Gooi's horizontal Movement (left and right arrows)

---
+++ Gooi Game (2) (2)/Assets/Sphere.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        print("What is this place? What am I? Who am I?");	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);	
+
+    }
+}
