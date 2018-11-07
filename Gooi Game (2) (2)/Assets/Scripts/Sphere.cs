c2ebca84ff3b388156b473d86d3c5eabedf40a4f
conneredgell <conneredgell5@gmail.com>
2018-11-06 20:28:43 -0500

    Boys Room Update
    I gave all the objects in the boys room a collider (some have a mesh collider and it messes Gooi up when he touches it. Hoping to figure out that bug soon). I have also added Gooi to the boys room and I made a script for the camera that follows gooi the correct way (not just a child of Gooi).

---
+++ Gooi Game (2) (2)/Assets/Scripts/Sphere.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour {
+
+    public float moveSpeed = 6;
+
+	// Use this for initialization
+	void Start () 
+    {
+        moveSpeed = 1f;
+
+        print("What is this place? What am I? Who am I?");	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);	
+
+    }
+}
