298f5d0c8f7298eb890335c2fac6ecf0ed1445aa
conneredgell <conneredgell5@gmail.com>
2018-12-04 16:31:00 -0500

    collectible pick up
    I created a simple object to test the collectibles. I then made an empty object with a box collider on it big enough for Gooi to fit into. Then I wrote some code that would only allow you to pick up the object (with space) when you are in the box. Still trying to figure out how to make a little text box pop up when you are able to pick up the object.

---
+++ Gooi Game (2) (2)/Assets/Scripts/PickUp.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    public GameObject collectible;
+
+    public bool canPickUp = false;
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+        collectible.SetActive(true);	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canPickUp)
+        {
+            collectible.SetActive(false);
+            Destroy(collectible);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (tag == "Player")
+        {
+            canPickUp = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(tag == "Player")
+        {
+            canPickUp = false;
+        }
+    }
+}
