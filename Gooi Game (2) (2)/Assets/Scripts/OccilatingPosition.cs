6a6fe0e9fcc4201810fdfc3ee19494a65dfcd6a9
Caroline Dunaway <dunawaycaroline@gmail.com>
2019-04-19 11:31:32 -0400

    Adding Animation

---
+++ Gooi Game (2) (2)/Assets/Scripts/OccilatingPosition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OccilatingPosition : MonoBehaviour
+{
+    public enum OccilationFuntion { Sine, Cosine }
+
+    public void Start()
+    {
+        //to start at zero
+        StartCoroutine(Oscillate(OccilationFuntion.Sine, 1f));
+        //to start at scalar value
+        //StartCoroutine (Oscillate (OccilationFuntion.Cosine, 1f));
+    }
+
+    private IEnumerator Oscillate(OccilationFuntion method, float scalar)
+    {
+        while (true)
+        {
+            if (method == OccilationFuntion.Sine)
+            {
+                transform.position = new Vector3(Mathf.Sin(Time.time) * scalar, 0, 0);
+            }
+            else if (method == OccilationFuntion.Cosine)
+            {
+                transform.position = new Vector3(Mathf.Cos(Time.time) * scalar, 0, 0);
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
